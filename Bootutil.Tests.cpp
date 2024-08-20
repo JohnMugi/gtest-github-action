aa9c760e3d43cee9d52bffd9f386d06a86dc0560
JohnMugi <mugi4501@gmail.com>
2024-08-20 16:11:19 +0530

    Update Bootutil.Tests.cpp

--- Bootutil.Tests.cpp
+++ Bootutil.Tests.cpp
@@ -18,7 +18,7 @@ TEST(bootutilTests, jumptoapp) {
     //act
     int expected = 0x0002;
     int actual = bootutil();
-    ASSERT_NQ(expected,actual);
+    ASSERT_NE(expected,actual);
     
     //assert
 }
