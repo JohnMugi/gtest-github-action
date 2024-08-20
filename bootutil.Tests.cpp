e49332636b8ceea79226130ceb9653b798c18703
JohnMugi <mugi4501@gmail.com>
2024-08-20 15:47:08 +0530

    Create bootutil.Tests.cpp

---
+++ bootutil.Tests.cpp
@@ -0,0 +1,14 @@
+#include <gtest/gtest.h>
+#include "bootutil.h"
+
+
+TEST(bootutilTests, jumptofbl) {
+    //arange
+    
+    //act
+    int expected = 0x0001;
+    int actual = bootutil();
+    //assert
+}
+
+
